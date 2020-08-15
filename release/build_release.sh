05e7c49e9356badd8d603135e63ef5e78745c731
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2020-08-15 15:46:30 -0700

    Now building tar gz

--- release/build_release.sh
+++ release/build_release.sh
@@ -3,3 +3,9 @@ set -exo pipefail
 
 docker build -t wink_builder release
 docker run -it --rm -v `pwd`:/work wink_builder /bin/bash -c "cd /work; cargo build --release --target armv5te-unknown-linux-musleabi"
+
+(rm -rf target/pkg || true) && mkdir -p target/pkg/opt/wink-mqtt-rs/
+cp release/* target/pkg/opt/wink-mqtt-rs/
+rm target/pkg/opt/wink-mqtt-rs/build_release.sh
+cp target/armv5te-unknown-linux-musleabi/release/wink-mqtt-rs target/pkg/opt/wink-mqtt-rs/
+tar -zcvf target/wink-mqtt-rs.tar.gz -C target/pkg .
