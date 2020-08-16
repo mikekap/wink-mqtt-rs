c97b11cb572bb20e6d3ec36c4aaa4bf54dce7808
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2020-08-15 17:24:29 -0700

    I hate permissions (#7)

--- release/build_release.sh
+++ release/build_release.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -exo pipefail
 
+(rm -rf target/pkg || true) && mkdir -p target/pkg/opt/wink-mqtt-rs/
+
 docker build -t wink_builder release
 docker run --rm -v `pwd`:/work wink_builder /bin/bash -c "cd /work; cargo build --release --target armv5te-unknown-linux-musleabi"
 
-(rm -rf target/pkg || true) && mkdir -p target/pkg/opt/wink-mqtt-rs/
 cp release/* target/pkg/opt/wink-mqtt-rs/
 rm target/pkg/opt/wink-mqtt-rs/build_release.sh
 cp target/armv5te-unknown-linux-musleabi/release/wink-mqtt-rs target/pkg/opt/wink-mqtt-rs/
