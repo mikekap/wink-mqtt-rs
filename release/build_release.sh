563872f055e6ab280a2ac8135921b56cc0d279b2
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2020-08-15 17:40:33 -0700

    Update build_release.sh

--- release/build_release.sh
+++ release/build_release.sh
@@ -7,6 +7,6 @@ docker build -t wink_builder release
 docker run --rm -v `pwd`:/work wink_builder /bin/bash -c "cd /work; cargo build --release --target armv5te-unknown-linux-musleabi"
 
 cp release/* target/pkg/opt/wink-mqtt-rs/
-rm target/pkg/opt/wink-mqtt-rs/build_release.sh
+rm target/pkg/opt/wink-mqtt-rs/{build_release.sh,Dockerfile}
 cp target/armv5te-unknown-linux-musleabi/release/wink-mqtt-rs target/pkg/opt/wink-mqtt-rs/
 tar -zcvf target/wink-mqtt-rs.tar.gz -C target/pkg .
