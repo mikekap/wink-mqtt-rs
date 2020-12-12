be1a3b88ca4750c416ecaaf75f09c62f4e0abc95
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2020-12-12 14:42:44 -0800

    fix the new extension

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,40 @@
-use slog::warn;
+use slog::{warn, error, info, debug, trace, crit, Level};
 use slog_scope;
 
 pub(crate) trait ResultExtensions<T, E> {
-    fn log_failing_result(self, message: &str) -> Option<T>;
+    fn log_failing_result_at(self, level: Level, message: &str) -> Option<T> where E: std::fmt::Debug;
+    fn log_failing_result(self, message: &str) -> Option<T> where Self: Sized, E: std::fmt::Debug {
+        self.log_failing_result_at(Level::Warning, message)
+    }
 }
 
-impl<T, E> ResultExtensions<T, E> for Result<T, E> {
-    fn log_failing_result(self, message: &str) -> Option<T> {
-        warn!(slog_scope::logger(), "{}", message; "error" => format!("{:?}", message));
-        self.ok()
+impl<T, E: std::fmt::Debug> ResultExtensions<T, E> for Result<T, E> {
+    fn log_failing_result_at(self, level: Level, message: &str) -> Option<T> {
+        match self {
+            Ok(v) => Some(v),
+            Err(e) => {
+                match level {
+                    Level::Warning => {
+                        warn!(slog_scope::logger(), "{}", message; "error" => ?e);
+                    },
+                    Level::Error => {
+                        error!(slog_scope::logger(), "{}", message; "error" => ?e);
+                    },
+                    Level::Critical => {
+                        crit!(slog_scope::logger(), "{}", message; "error" => ?e);
+                    }
+                    Level::Info => {
+                        info!(slog_scope::logger(), "{}", message; "error" => ?e);
+                    }
+                    Level::Debug => {
+                        debug!(slog_scope::logger(), "{}", message; "error" => ?e);
+                    }
+                    Level::Trace => {
+                        trace!(slog_scope::logger(), "{}", message; "error" => ?e);
+                    }
+                }
+                None
+            }
+        }
     }
 }
