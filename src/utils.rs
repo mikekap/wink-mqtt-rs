6d97c337b843091b54348eb276ca37ba9c473624
Mike Kaplinskiy <mike.kaplinskiy@gmail.com>
2020-12-12 14:48:56 -0800

    cargo fmt

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,15 @@
-use slog::{warn, error, info, debug, trace, crit, Level};
+use slog::{crit, debug, error, info, trace, warn, Level};
 use slog_scope;
 
 pub(crate) trait ResultExtensions<T, E> {
-    fn log_failing_result_at(self, level: Level, message: &str) -> Option<T> where E: std::fmt::Debug;
-    fn log_failing_result(self, message: &str) -> Option<T> where Self: Sized, E: std::fmt::Debug {
+    fn log_failing_result_at(self, level: Level, message: &str) -> Option<T>
+    where
+        E: std::fmt::Debug;
+    fn log_failing_result(self, message: &str) -> Option<T>
+    where
+        Self: Sized,
+        E: std::fmt::Debug,
+    {
         self.log_failing_result_at(Level::Warning, message)
     }
 }
@@ -16,10 +22,10 @@ impl<T, E: std::fmt::Debug> ResultExtensions<T, E> for Result<T, E> {
                 match level {
                     Level::Warning => {
                         warn!(slog_scope::logger(), "{}", message; "error" => ?e);
-                    },
+                    }
                     Level::Error => {
                         error!(slog_scope::logger(), "{}", message; "error" => ?e);
-                    },
+                    }
                     Level::Critical => {
                         crit!(slog_scope::logger(), "{}", message; "error" => ?e);
                     }
